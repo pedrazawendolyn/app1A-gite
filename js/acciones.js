02d4f45d381ef1822db0f1de0e54cc9f8324e1ff
pedrazawendolyn <WENDOLYNPEDRAZA.CBTIS16@gmail.com>
2018-04-13 09:03:32 -0500

    5

--- js/acciones.js
+++ js/acciones.js
@@ -27,5 +27,5 @@ $(document).ready(function (e){
 });//document
 
 function escribehistoria(accion){
-	$('#eHistoria').append('<li>'+accion+'<li>');
+	$('#eHistoria').append('<li>'+accion+'</li>');
 }
